8256827434809cbc88c3bd1ac0822e0bb963ca87
756445638 <756445638@qq.com>
2016-10-25 11:00:01 +0800

    add LICENSE

--- master/v1slave.go
+++ master/v1slave.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2013 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package master
 
 import (
@@ -135,9 +150,7 @@ func (v1s *V1Slave) timeoutTick() {
 }
 
 func (v1s *V1Slave) writeLoop() error {
-	fmt.Println("write loop")
 	for job := range v1s.jobschan {
-		fmt.Println("###", string(job.c.Command), job.jobid, job.c.Parameter)
 		v1s.conn.SetDeadline(time.Now().Add(time.Second * 30))
 		_, err := job.c.Write(v1s.conn)
 		if err != nil {
--- slave.go
+++ slave.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2013 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
@@ -18,11 +33,11 @@ var (
 
 func main() {
 	flag.Parse()
-	if *worker <= 3 {
-		*worker = 3
+	if *worker <= 1 {
+		*worker = 1
 	}
-	if *worker > 30 {
-		*worker = 30
+	if *worker > 5 {
+		*worker = 5
 	}
 	wg.Add(*worker)
 	for i := 0; i < *worker; i++ {
